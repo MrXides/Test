cd76bfce0b295096026a498c547700a81344540b
Sergey Smirnov <sergey.i.smirnov@rt.ru>
2022-08-18 14:10:15 +0300

    T3

--- src/test/java/ru/rtkit/someservice/Tests/SomeTest.java
+++ src/test/java/ru/rtkit/someservice/Tests/SomeTest.java
@@ -1,4 +1,40 @@
 package ru.rtkit.someservice.Tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.TmsLink;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.rtkit.someservice.apiHelper.Endpoints;
+
+import static ru.rtkit.someservice.Tests.BaseTest.apiHelper;
+import static ru.rtkit.someservice.Tests.BaseTest.resp200;
+
 public class SomeTest {
+    @Test
+    @DisplayName("Find pet")
+    @Description("Поиск питомцев")
+    @TmsLink("125")
+    void findPetByID() {
+        String petName = "ЧикЧирик";
+        JSONObject reqBody = new JSONObject()
+                .put("id", 0)
+                .put("category", new JSONObject().put("name", "birds"))
+                .put("name", petName)
+                .put("status", "available");
+        long petId = addNewPetToStore(reqBody.toString(), petName);
+        String actualName = apiHelper.get(Endpoints.PET_ID, resp200, petId)
+                .jsonPath()
+                .getString("name");
+        Assertions.assertEquals(petName, actualName, "Ошибка при проверке имени созданного животного");
+    }
+
+    @Step("Добавление питомца {petNames} в магазин ")
+    long addNewPetToStore(String pet, String petNames) {
+        return apiHelper.post(Endpoints.NEW_PET, pet, resp200)
+                .jsonPath()
+                .getLong("id");
+    }
 }
---
+++ src/test/java/ru/rtkit/someservice/apiHelper/Endpoints.java
@@ -0,0 +1,15 @@
+package ru.rtkit.someservice.apiHelper;
+
+public class Endpoints {
+    public static final String NEW_PET = "/pet";
+    public static final String PET_ID = "/pet/{petID}";
+
+    public static final String INVENTORY = "/store/inventory";
+    public static final String ORDER = "/store/order";
+
+    public static final String USER = "/user";
+    public static final String USERNAME = "/user/{username}";
+    public static final String USER_LOGIN = "/user/login";
+
+
+}
